f1f62e79d54708351ae97091d91ca9b4a5170e53
hyojining <102501038+hyojining@users.noreply.github.com>
2023-10-21 21:06:15 +0900

    tabBox1 add comments

--- ch05/src/main/webapp/js/tabBox.js
+++ ch05/src/main/webapp/js/tabBox.js
@@ -6,20 +6,26 @@ var tabBox = {
 		var elLis = elUl.getElementsByTagName('li');
 		
 		Array.prototype.forEach.call(elLis, function(li) {
+			// 이 함수 내의 this는 현재 컨텍스트인 tabBox 객체를 가리킨다.
+			// onTabClicked 메서드를 바인딩된 this 컨텍스트로 실행한다.
+			// 이때, this는 tabBox 객체를 가리키게 된다.
 			li.onclick = this.onTabClicked.bind(this);
-		}.bind(this));
+		}.bind(this)); // 내부 함수에서의 this를 tabBox 객체로 설정한다.
 	},
 	init: function() {
-		window.onload = this._init.bind(this);
+		// _init 메서드를 현재 객체('tabBox' 객체)인 this에 바인딩한다.
+		// 이렇게 하면, _init 메서드 내에서 this를 사용할 때 tabBox 객체를 참조하게 된다.
+		window.onload = this._init.bind(this); 
 	},
 	onTabClicked: function(e) {
-		
+		// tabBox 객체 내의 프로퍼티 참조 -> this.liSelected
 		if(this.liSelected != null) {
 			this.liSelected.className = '';
 		}
 		
+		// e: 이벤트 객체
+		// e.target: 클릭 이벤트가 발생한 HTML 요소
 		e.target.className='selected';
-		this.liSelected = e.target;
-		
+		this.liSelected = e.target;	
 	}
 };
